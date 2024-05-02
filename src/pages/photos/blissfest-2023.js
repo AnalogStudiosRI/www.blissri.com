6fc286bfaf8e979ee0eb6bce33c450cb07956697
Owen Buckley <owen@thegreenhouse.io>
2024-05-02 14:45:20 -0400

    stack height of 2

--- src/pages/photos/blissfest-2023.js
+++ src/pages/photos/blissfest-2023.js
@@ -1,7 +1,10 @@
 import { getAssetsByFolder } from '../../services/cloudinary-service.js';
 
+// masonry layout curtesy of
+// https://flowbite.com/docs/components/gallery/
 export default class PhotoPage extends HTMLElement {
   async connectedCallback() {
+    const STACK_HEIGHT = 2;
     const assets = await getAssetsByFolder('Blissfest 2023');
 
     this.innerHTML = `
@@ -9,35 +12,50 @@ export default class PhotoPage extends HTMLElement {
         Blissfest 2023
       </h1>
 
-      ${
-        assets.map((asset) => {
-          const { format, height, width, publicId, resourceType } = asset;
+      <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+        ${
+          assets.map((asset, idx) => {
+            const { format, height, width, publicId, resourceType } = asset;
+            const isStartOfGroup = idx % STACK_HEIGHT === 0;
+            const isEndOfGroup = (idx + 1) % STACK_HEIGHT === 0 && idx !== 0 || idx + 1 === assets.length;
 
-          if (resourceType === 'image') {
-            return `
-                <img
-                height="${height}"
-                width="${width}"
-                style="width: 50%; margin: 0 auto; border: 1px dotted black"
-                src="https://res.cloudinary.com/dmswtygse/image/upload/c_scale,h_400/f_auto/${publicId}"
-                alt="Blissfest 2023"
-                loading="lazy"
-              />`
-            ;
-          } else if (resourceType === 'video') {
-            return `
-              <video
-                controls
-                style="width: 50%; margin: 0 auto; border: 1px dotted black"
-              >
-                <source src="https://res.cloudinary.com/dmswtygse/video/upload/f_mp4/${publicId}" type="video/mp4" />
-              </video>
-            `;
-          } else {
-            console.debug('Unsupported format detected => ', { format, publicId });
-          }
-        }).join('')
-      }
-    `;
+            // console.log({ idx, isStartOfGroup, isEndOfGroup });
+            if (resourceType === 'image') {
+              return `
+                ${isStartOfGroup ? '<div class="grid gap-4">' : ''}
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    height="${height}"
+                    width="${width}"
+                    src="https://res.cloudinary.com/dmswtygse/image/upload/c_scale,h_400/f_auto/${publicId}"
+                    alt="Blissfest 2023"
+                    loading="lazy"
+                  />
+                </div>
+                ${isEndOfGroup ? '</div>' : ''}
+                `
+              ;
+            } else if (resourceType === 'video') {
+              return `
+                ${isStartOfGroup ? '<div class="grid gap-4">' : ''}
+                <div>
+                  <video
+                    controls
+                    class="h-auto max-w-full rounded-lg"
+                    loading="lazy"
+                  >
+                    <source src="https://res.cloudinary.com/dmswtygse/video/upload/c_scale,h_400/f_mp4/${publicId}" type="video/mp4" />
+                  </video>
+                </div>
+                ${isEndOfGroup ? '</div>' : ''}
+              `;
+            } else {
+              console.debug('Unsupported format detected => ', { format, publicId });
+            }
+          }).join('')
+        }
+      </div>
+     `;
   }
 }

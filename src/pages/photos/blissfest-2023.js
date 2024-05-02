9ad48ed2ffd051c7d7141afa647c8729ec10e1ba
Owen Buckley <owen@thegreenhouse.io>
2024-05-02 15:24:15 -0400

    media card component

--- src/pages/photos/blissfest-2023.js
+++ src/pages/photos/blissfest-2023.js
@@ -1,4 +1,5 @@
 import { getAssetsByFolder } from '../../services/cloudinary-service.js';
+import '../../components/media-card/media-card.js';
 
 // masonry layout curtesy of
 // https://flowbite.com/docs/components/gallery/
@@ -15,44 +16,21 @@ export default class PhotoPage extends HTMLElement {
       <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
         ${
           assets.map((asset, idx) => {
-            const { format, height, width, publicId, resourceType } = asset;
             const isStartOfGroup = idx % STACK_HEIGHT === 0;
             const isEndOfGroup = (idx + 1) % STACK_HEIGHT === 0 && idx !== 0 || idx + 1 === assets.length;
 
-            // console.log({ idx, isStartOfGroup, isEndOfGroup });
-            if (resourceType === 'image') {
-              return `
-                ${isStartOfGroup ? '<div class="grid gap-4">' : ''}
-                <div>
-                  <img
-                    class="h-auto max-w-full rounded-lg"
-                    height="${height}"
-                    width="${width}"
-                    src="https://res.cloudinary.com/dmswtygse/image/upload/c_scale,h_400/f_auto/${publicId}"
-                    alt="Blissfest 2023"
-                    loading="lazy"
-                  />
-                </div>
-                ${isEndOfGroup ? '</div>' : ''}
-                `
-              ;
-            } else if (resourceType === 'video') {
-              return `
-                ${isStartOfGroup ? '<div class="grid gap-4">' : ''}
-                <div>
-                  <video
-                    controls
-                    class="h-auto max-w-full rounded-lg"
-                    loading="lazy"
-                  >
-                    <source src="https://res.cloudinary.com/dmswtygse/video/upload/c_scale,h_400/f_mp4/${publicId}" type="video/mp4" />
-                  </video>
-                </div>
-                ${isEndOfGroup ? '</div>' : ''}
-              `;
-            } else {
-              console.debug('Unsupported format detected => ', { format, publicId });
-            }
+            return `
+              ${isStartOfGroup ? '<div class="grid gap-4">' : ''}
+
+              <div>
+                <app-media-card 
+                  class="h-auto max-w-full rounded-lg"
+                  asset='${JSON.stringify({ ...asset, alt: 'Blissfest 2023' }).replace(/'/g, '\\"')}'
+                /></app-media-card>
+              </div>
+
+              ${isEndOfGroup ? '</div>' : ''}
+            `;
           }).join('')
         }
       </div>
